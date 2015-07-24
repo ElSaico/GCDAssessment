72392b6a1a428debe90dd5e6741e9f7eabce89da
Bruno Marques <bruno@uberwald-dev.net>
2015-07-23 22:44:45 -0300

    Tidy data set

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,22 @@
-download.file('https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip', 'har.zip')
+library(dplyr)
 
-features <- read.table(unz('har.zip', 'UCI HAR Dataset/features.txt'), row.names=1, stringsAsFactors=FALSE, header=FALSE)[,]
+features <- read.table(unz('har.zip', 'UCI HAR Dataset/features.txt'), row.names=1, header=FALSE)[,]
 activity <- read.table(unz('har.zip', 'UCI HAR Dataset/activity_labels.txt'), row.names=1, header=FALSE)[,]
 
-train <- read.table(unz('har.zip', 'UCI HAR Dataset/train/X_train.txt'), col.names=features, header=FALSE)
+train <- read.table(unz('har.zip', 'UCI HAR Dataset/train/X_train.txt'), header=FALSE)
+test <- read.table(unz('har.zip', 'UCI HAR Dataset/test/X_test.txt'), header=FALSE)
+names(train) <- features
+names(test) <- features
+
 train.activity <- read.table(unz('har.zip', 'UCI HAR Dataset/train/y_train.txt'), header=FALSE)[,]
-train$subject <- read.table(unz('har.zip', 'UCI HAR Dataset/train/subject_train.txt'), header=FALSE)[,]
-train$activity <- factor(train.activity, labels=activity)
-
-test <- read.table(unz('har.zip', 'UCI HAR Dataset/test/X_test.txt'), col.names=features, header=FALSE)
 test.activity <- read.table(unz('har.zip', 'UCI HAR Dataset/test/y_test.txt'), header=FALSE)[,]
-test$subject <- read.table(unz('har.zip', 'UCI HAR Dataset/test/subject_test.txt'), header=FALSE)[,]
-test$activity <- factor(test.activity, labels=activity)
 
-columns <- features[grep("(mean|std)\\(\\)", features)]
-columns <- append(columns, c("activity", "subject"))
-data <- rbind(train, test)[,columns] # broken because God knows why
-#data.metrics <- ???
+train.subject <- read.table(unz('har.zip', 'UCI HAR Dataset/train/subject_train.txt'), header=FALSE)[,]
+test.subject <- read.table(unz('har.zip', 'UCI HAR Dataset/test/subject_test.txt'), header=FALSE)[,]
+
+data <- rbind(train, test)[,grep("(mean|std)\\(\\)", features)]
+data$activity <- factor(c(train.activity, test.activity), labels=activity)
+data$subject <- c(train.subject, test.subject)
+data.grouped <- group_by(data, activity, subject)
+data.averages <- summarise_each(data.grouped, funs(mean))
+write.table(data.averages, 'har_averages.txt', row.name=FALSE)
